f4aa7f5b65eaebfc803f0a9284bbca6cea5b2c86
Esperanza González <esperanza@mechaphysis.org>
2019-08-01 12:01:09 +0200

    feat: change styling palette to teal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 //Pages:
 import home from "./modules/home/home";
@@ -9,23 +10,36 @@ import signup from "./modules/signup/signup";
 
 //Molecule NavBar
 import NavBar from "./components/molecules/NavBar";
+import { teal } from "@material-ui/core/colors";
+import { MuiThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: {
+      main: "#96000F"
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>Barebones App </h1>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <h1>Barebones App </h1>
 
-      <Router>
-        <NavBar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+        <Router>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 

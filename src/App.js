1eb215a2f3968c52e37a5dc591178874f21c0742
Esperanza González <esperanza@mechaphysis.org>
2019-08-01 11:31:10 +0200

    feat: create basic pages structures

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+//Pages:
+import home from "./modules/home/home";
+import login from "./modules/login/login";
+import signup from "./modules/signup/signup";
+
+//Molecule NavBar
+import NavBar from "./components/molecules/NavBar";
 
 function App() {
   return (
     <div className="App">
       <h1>Barebones App </h1>
+
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }

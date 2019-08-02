331417944440c3c055caf175dfc99e193dd9840b
Esperanza González <esperanza@mechaphysis.org>
2019-08-02 16:27:39 +0200

    feat: change format date to relative 'fromNow'

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+//For date formatting:
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
 //Material UI
 import WithStyles from "@material-ui/styles/WithStyles";
 import Card from "@material-ui/core/Card";
@@ -22,6 +26,9 @@ const styles = {
     padding: 25
   }
 };
+
+//Extend dayjs to show date in format 'X days ago'
+dayjs.extend(relativeTime);
 
 function Post(props) {
   const {
@@ -53,7 +60,7 @@ function Post(props) {
           {userHandle}
         </Typography>
         <Typography variant="body2" color="textSecondary">
-          {createdAt}
+          {dayjs(createdAt).fromNow()}
         </Typography>
         <Typography variant="body1">{body}</Typography>
       </CardContent>

dd09c40c56a60c247c03befe2a50372fe4174eab
Esperanza González <esperanza@mechaphysis.org>
2019-08-02 10:50:40 +0200

    feat: show all posts

---
+++ src/components/Post.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Link from "react-router-dom/Link";
+
+//Material UI
+import WithStyles from "@material-ui/styles/WithStyles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+//Styles in JSS manner:
+const styles = {
+  card: {
+    display: "flex",
+    marginBottom: 20
+  },
+  image: {
+    minWidth: 200,
+    objectFit: "cover"
+  },
+  content: {
+    padding: 25
+  }
+};
+
+function Post(props) {
+  const {
+    classes,
+    post: {
+      postId,
+      likeCount,
+      commentCount,
+      userImage,
+      body,
+      createdAt,
+      userHandle
+    }
+  } = props;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.image}
+        image={userImage}
+        title="Profile pic"
+      />
+      <CardContent className={classes.content}>
+        <Typography
+          variant="h5"
+          color="primary"
+          component={Link}
+          to={`/users/${userHandle}`}
+        >
+          {userHandle}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          {createdAt}
+        </Typography>
+        <Typography variant="body1">{body}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default WithStyles(styles)(Post);
--- src/modules/home/home.js
+++ src/modules/home/home.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import { fetchPosts } from "../../services/postsService";
+import Post from "../../components/Post";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+class Home extends Component {
+  state = {
+    posts: null
+  };
+  componentDidMount() {
+    fetchPosts().then(postsData => {
+      this.setState({ posts: postsData });
+      console.log("---> print postsData: ", postsData);
+    });
+  }
+  render() {
+    let recentPosts =
+      this.state.posts !== null ? (
+        this.state.posts.map(post => <Post post={post} />)
+      ) : (
+        <p>Loading...</p>
+      );
+    return (
+      <Grid container spacing={6}>
+        <Grid item sm={8} xs={12}>
+          {recentPosts}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile</p>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default Home;

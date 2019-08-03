ce1bd8b249201fff7c6689b81387fc6f7d2013be
Esperanza González <esperanza@mechaphysis.org>
2019-08-03 22:04:10 +0200

    feat: add helper functions for checking user authentication and token expiration

--- src/utils/userAuth.js
+++ src/utils/userAuth.js
@@ -1,3 +1,4 @@
+import jwtDecode from "jwt-decode";
 import { EMPTY_OBJECT_READONLY } from "../constants/emptyDefaults";
 import { AUTH_DETAILS_KEY } from "../constants/auth";
 
@@ -5,4 +6,18 @@ export const setUserAuthDetailsInLS = token =>
   localStorage.setItem(AUTH_DETAILS_KEY, `Bearer ${token}`);
 
 export const getUserAuthDetailsFromLS = () =>
-  localStorage.getItem(AUTH_DETAILS_KEY || EMPTY_OBJECT_READONLY);
+  localStorage.getItem(AUTH_DETAILS_KEY) || EMPTY_OBJECT_READONLY;
+
+export const isUserAuthenticated = () => {
+  const userAuthDetails = getUserAuthDetailsFromLS();
+  return {
+    isAuthenticated: !!userAuthDetails && !!userAuthDetails.length
+  };
+};
+
+export const isSessionExpired = () => {
+  const userAuthToken = getUserAuthDetailsFromLS().split(" ")[1];
+
+  const decodedToken = jwtDecode(userAuthToken);
+  return !!(decodedToken.exp * 1000 < Date.now());
+};

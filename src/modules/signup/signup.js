73808aa8a07e3d0c9000b0e806b7b32a7ca09091
Esperanza González <esperanza@mechaphysis.org>
2019-08-03 14:34:14 +0200

    feat: create signup page

--- src/modules/signup/signup.js
+++ src/modules/signup/signup.js
@@ -1,11 +1,138 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function SignUp() {
+import { includes } from "ramda";
+
+//Service:
+import { signUpUser } from "../../services/signUpService";
+//Auth helpers:
+import { setUserAuthDetailsInLS } from "../../utils/userAuth";
+
+//Material UI:
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+//Styling using JSS:
+import { formStyles } from "../../styles/general";
+const styles = formStyles;
+
+let formFields = ["handle", "email", "password", "confirmPassword"];
+
+const SignUp = props => {
+  //Using hooks for state:
+  const [form, setValues] = useState({
+    handle: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({
+    handle: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    general: ""
+  });
+  const redirectToHome = () => props.history.push("/");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setLoading(true);
+    signUpUser(form)
+      .then(data => {
+        console.log("--> data: ", data);
+        setUserAuthDetailsInLS(data.token);
+        setLoading(false);
+        redirectToHome();
+      })
+      .catch(error => {
+        console.log("-->", error);
+        setLoading(false);
+        //setErrors({ ...error.response.data.errors });
+      });
+  };
+
+  const handleChange = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const renderTextField = fieldname => {
+    return (
+      <TextField
+        key={fieldname}
+        id={fieldname}
+        name={fieldname}
+        type={includes("confirm", fieldname) ? "password" : fieldname}
+        label={includes("confirm", fieldname) ? "confirm password" : fieldname}
+        className={classes.textField}
+        value={form[fieldname]}
+        helperText={errors[fieldname]}
+        error={errors[fieldname] ? true : false}
+        onChange={handleChange}
+        fullWidth
+      />
+    );
+  };
+
+  let { classes } = props; //For the JSS styling
+  const renderGeneralErrors = errors => {
+    return errors.general ? (
+      <Typography variant="body2" className={classes.customError}>
+        {errors.general}
+      </Typography>
+    ) : null;
+  };
+
   return (
-    <div>
-      <h1>Sign up</h1>
-    </div>
+    <Grid container className={classes.form}>
+      <Grid item sm />
+      <Grid item sm>
+        <Typography variant="h3" className={classes.pageTitle}>
+          Sign Up
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          {formFields.map(field => renderTextField(field))}
+          {renderGeneralErrors(errors)}
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={loading}
+            className={classes.button}
+          >
+            Sign Up
+            {loading && (
+              <CircularProgress size={30} className={classes.progress} />
+            )}
+          </Button>
+          <Grid container>
+            <Grid item className={classes.center}>
+              <Typography variant="body2">
+                Already registered?{" "}
+                <Link to="/login" variant="body2">
+                  {"Sign In"}
+                </Link>
+              </Typography>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+      <Grid item sm />
+    </Grid>
   );
-}
+};
 
-export default SignUp;
+SignUp.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SignUp);

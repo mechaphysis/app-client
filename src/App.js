5891543f533e032bd12fc07a99c7b5b9f34de199
Esperanza González <esperanza@mechaphysis.org>
2019-08-03 22:04:30 +0200

    feat: handle token expiration

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import home from "./modules/home/home";
 import login from "./modules/login/Login";
 import signup from "./modules/signup/SignUp";
+//Auth helpers:
+import { isSessionExpired } from "./utils/userAuth";
 
 //Molecule NavBar
 import NavBar from "./components/molecules/NavBar";
 import { MuiThemeProvider } from "@material-ui/core";
 
 const theme = createMuiTheme(customTheme);
+
+const sessionHasExpired = isSessionExpired();
+if (sessionHasExpired) {
+  window.location.href = "/login";
+}
 
 function App() {
   return (

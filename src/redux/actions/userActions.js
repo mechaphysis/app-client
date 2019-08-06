d1cfd16f24bbecc53e18570c916de6dcfd6b57bc
Esperanza González <esperanza@mechaphysis.org>
2019-08-06 17:24:21 +0200

    feat: define actions for signup and logout

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,19 +1,54 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../actionTypes";
+import {
+  SET_USER,
+  SET_UNAUTHENTICATED,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI
+} from "../actionTypes";
 
 //Service:
-import { loginService } from "../../services/loginService";
-import { userService } from "../../services/userService";
+import {
+  signUpService,
+  loginService,
+  getUserService
+} from "../../services/userService";
 
 //Auth helpers:
 import {
   setUserAuthDetailsInLS,
-  setAuthorizationHeader
+  setAuthorizationHeader,
+  removeUserAuthDetailsFromLS,
+  removeAuthorizationHeader
 } from "../../utils/userAuth";
+
+export const signUpUser = (form, history) => dispatch => {
+  dispatch({
+    type: LOADING_UI
+  });
+  console.log("--->user action");
+  signUpService(form)
+    .then(data => {
+      setUserAuthDetailsInLS(data.token);
+      setAuthorizationHeader(data.token);
+      dispatch(fetchUserData());
+      dispatch({
+        type: CLEAR_ERRORS
+      });
+      history.push("/");
+    })
+    .catch(error => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: error.response.data.errors
+      });
+    });
+};
 
 export const loginUser = (form, history) => dispatch => {
   dispatch({
     type: LOADING_UI
   });
+  console.log("--->user action");
 
   loginService(form)
     .then(data => {
@@ -34,7 +69,7 @@ export const loginUser = (form, history) => dispatch => {
 };
 
 export const fetchUserData = () => dispatch => {
-  userService()
+  getUserService()
     .then(userData => {
       dispatch({
         type: SET_USER,
@@ -42,4 +77,12 @@ export const fetchUserData = () => dispatch => {
       });
     })
     .catch(error => console.log("Something went wrong: ", error));
+};
+
+export const logOutUser = () => dispatch => {
+  removeUserAuthDetailsFromLS();
+  removeAuthorizationHeader();
+  dispatch({
+    type: SET_UNAUTHENTICATED
+  });
 };

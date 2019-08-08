15d3bac55e5f9554eaffd66b6c710f9e0ff85d69
Esperanza González <esperanza@mechaphysis.org>
2019-08-08 22:07:59 +0200

    feat: load posts using redux hooks

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -1,43 +1,37 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
-import { fetchPosts } from "../../../services/postsService";
+import { getPosts } from "../../../redux/actions/dataActions";
 import Post from "../../organisms/post/Post";
 import Profile from "../../organisms/profile/Profile";
 
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
-class Home extends Component {
-  state = {
-    posts: null
-  };
+const Home = () => {
+  //Connect to Redux store using hooks
+  const dispatch = useDispatch();
+  const posts = useSelector(store => store.data.posts);
+  const loading = useSelector(store => store.data.loading);
 
-  componentDidMount() {
-    fetchPosts().then(postsData => {
-      this.setState({ posts: postsData });
-    });
-  }
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
 
-  render() {
-    let recentPosts =
-      this.state.posts !== null ? (
-        this.state.posts.map(post => <Post key={post.postId} post={post} />)
-      ) : (
-        <p>Loading...</p>
-      );
-    return (
-      <Grid container spacing={6}>
-        <Grid item sm={8} xs={12}>
-          {recentPosts}
-        </Grid>
-        <Grid item sm={4} xs={12}>
-          <Profile />
-        </Grid>
+  let recentPosts = loading ? (
+    <p>Loading...</p>
+  ) : (
+    posts.map(post => <Post key={post.postId} post={post} />)
+  );
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item sm={8} xs={12}>
+        {recentPosts}
       </Grid>
-    );
-  }
-}
+      <Grid item sm={4} xs={12}>
+        <Profile />
+      </Grid>
+    </Grid>
+  );
+};
 
-const mapStateToProps = state => ({
-  isAuthenticated: state.user.isAuthenticated
-});
-export default connect(mapStateToProps)(Home);
+export default Home;

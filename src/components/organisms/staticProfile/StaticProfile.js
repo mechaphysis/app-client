3fce1cb1a2af9e185e652344e4e56fa10471166a
Esperanza González <esperanza@mechaphysis.org>
2019-08-22 13:40:05 +0200

    feat: define structure of basic static profile

--- src/components/organisms/staticProfile/StaticProfile.js
+++ src/components/organisms/staticProfile/StaticProfile.js
@@ -1,11 +1,12 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import withStyles from "@material-ui/core/styles/withStyles";
 import dayjs from "dayjs";
 
-import Link from "@material-ui/core/Link";
+import MuiLink from "@material-ui/core/Link";
+import Avatar from "@material-ui/core/Avatar";
 import Paper from "@material-ui/core/Paper";
-import Typograhpy from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 import LocationOn from "@material-ui/icons/LocationOn";
 import LinkIcon from "@material-ui/icons/Link";
 import CalendarToday from "@material-ui/icons/CalendarToday";
@@ -25,8 +26,50 @@ const styles = {
   }
 };
 
-const StaticProfile = () => {
-  return <div />;
+const StaticProfile = props => {
+  const {
+    classes,
+    profile: { handle, createdAt, imageUrl, bio, website, location }
+  } = props;
+
+  return (
+    <Paper className={classes.paper}>
+      <div className={classes.profile}>
+        <Avatar alt={`Profile`} src={imageUrl} className={classes.bigAvatar} />
+        <MuiLink
+          component={Link}
+          to={`/users/${handle}`}
+          color="primary"
+          variant="h5"
+        >
+          @{handle}
+        </MuiLink>
+        <hr />
+        {bio && <Typography variant="body2">{bio}</Typography>}
+        <hr />
+        {location && (
+          <Fragment>
+            <LocationOn color="primary">
+              <span>{location}</span>
+            </LocationOn>
+            <hr />
+          </Fragment>
+        )}
+        {website && (
+          <Fragment>
+            <LinkIcon color="primary" />
+            <a href={website} target="_blank" rel="noopener noreferrer">
+              {" "}
+              {website}
+            </a>
+            <hr />
+          </Fragment>
+        )}
+        <CalendarToday color="primary" />{" "}
+        <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
+      </div>
+    </Paper>
+  );
 };
 
 export default withStyles(styles)(StaticProfile);

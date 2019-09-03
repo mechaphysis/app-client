9ec26d6d50501e94a4fe714004cc736bbecfa7eb
Esperanza González <esperanza@mechaphysis.org>
2019-09-03 16:35:13 +0200

    refactor: move skeletons styles to its own for global use

---
+++ src/styles/skeleton.js
@@ -0,0 +1,41 @@
+const skeletonStyles = {
+  card: {
+    display: "flex",
+    marginBottom: 20
+  },
+  cardContent: {
+    width: "100%",
+    flexDirection: "column",
+    padding: 25
+  },
+  cover: {
+    minWidth: 200,
+    objectFit: "cover"
+  },
+  handle: {
+    width: 60,
+    height: 18,
+    backgroundColor: "#009688",
+    marginBottom: 7
+  },
+  date: {
+    height: 14,
+    width: 100,
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
+    marginBottom: 10
+  },
+  fullLine: {
+    height: 15,
+    width: "90%",
+    marginBottom: 10,
+    backgroundColor: "rgba(0, 0, 0, 0.54)"
+  },
+  halfLine: {
+    height: 15,
+    width: "50%",
+    marginBottom: 10,
+    backgroundColor: "rgba(0, 0, 0, 0.54)"
+  }
+};
+
+export default skeletonStyles;
--- src/utils/PostSkeleton.js
+++ src/utils/PostSkeleton.js
@@ -7,48 +7,8 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-import { EMPTY_OBJECT_READONLY } from "../constants/emptyDefaults";
-import { palette } from "@material-ui/system";
-
-const styles = {
-  card: {
-    display: "flex",
-    marginBottom: 20
-  },
-  cardContent: {
-    width: "100%",
-    flexDirection: "column",
-    padding: 25
-  },
-  cover: {
-    minWidth: 200,
-    objectFit: "cover"
-  },
-  handle: {
-    width: 60,
-    height: 18,
-    backgroundColor: "#009688",
-    marginBottom: 7
-  },
-  date: {
-    height: 14,
-    width: 100,
-    backgroundColor: "rgba(0, 0, 0, 0.3)",
-    marginBottom: 10
-  },
-  fullLine: {
-    height: 15,
-    width: "90%",
-    marginBottom: 10,
-    backgroundColor: "rgba(0, 0, 0, 0.54)"
-  },
-  halfLine: {
-    height: 15,
-    width: "50%",
-    marginBottom: 10,
-    backgroundColor: "rgba(0, 0, 0, 0.54)"
-  }
-};
+import skeletonStyles from "../styles/skeleton";
+const styles = { ...skeletonStyles };
 
 const PostSkeleton = props => {
   const { classes } = props;
